20e21f575818302ed0b402d3fb1b980fdb34dc85
truxxu <danielfetrujillo@gmail.com>
2021-08-30 11:12:19 -0500

    created folder structure and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import styles from "./App.module.scss";
+import { Welcome, Success } from "./pages";
 
 function App() {
   return (
     <div className={styles.container}>
-      <p>Hello world</p>
+      <Router>
+        <Switch>
+          <Route path="/success">
+            <Success />
+          </Route>
+          <Route path="/">
+            <Welcome />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
